d8c80571d474289ea4c49be0238696ce7a45bf9f
iamdevcode <iamdevcode@gmail.com>
2024-07-27 12:17:57 -0400

    add buttom tabs and app screens

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,3 +1,5 @@
+import { Redirect } from 'expo-router'
+
 export default function HomeScreen() {
-  return <></>
+  return <Redirect href="(tabs)" />
 }
---
+++ src/context/app.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from 'react'
+
+const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+  const data = {}
+  return <AppContext.Provider value={data}>{children}</AppContext.Provider>
+}
+
+const useApp = () => useContext(AppContext)
+
+export { AppProvider, useApp }
